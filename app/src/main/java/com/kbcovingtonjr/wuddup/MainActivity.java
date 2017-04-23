41b8b420e663e94514d64bea576fb61d22c12dd3
Keith Blaine Covington Jr <kbcovingtonjr@gmail.com>
2017-04-22 18:35:37 -0600

    Now zooms to current location

--- app/src/main/java/com/kbcovingtonjr/wuddup/MainActivity.java
+++ app/src/main/java/com/kbcovingtonjr/wuddup/MainActivity.java
@@ -1,8 +1,14 @@
 package com.kbcovingtonjr.wuddup;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,17 +18,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-
+// import statements for Google Map API usage
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+// Other things to import
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements
+        NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleMap.OnCameraIdleListener {
+
+    private GoogleMap mMap;
+    private static final LatLngBounds ADELAIDE = new LatLngBounds(
+            new LatLng(-35.0, 138.58), new LatLng(-34.9, 138.61));
+    private static final CameraPosition ADELAIDE_CAMERA = new CameraPosition.Builder()
+            .target(new LatLng(-34.92873, 138.59995)).zoom(20.0f).bearing(0).tilt(0).build();
+
+    private static final LatLngBounds PACIFIC = new LatLngBounds(
+            new LatLng(-15.0, 165.0), new LatLng(15.0, -165.0));
+    private static final CameraPosition PACIFIC_CAMERA = new CameraPosition.Builder()
+            .target(new LatLng(0, -180)).zoom(4.0f).bearing(0).tilt(0).build();
+
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
+
+
+    /** HashMap containing user preference data */
+    private HashMap<String,String> dataIn = new HashMap<>();
+
+    /** HashMap containing user GPS info e.g. lat/long coordinates */
+    private HashMap<String,Integer> locationData = new HashMap<>();
+
+    /** HashMap containing place info e.g. type of event, lat/long coordinates */
+    private HashMap<String,Integer> places = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +93,15 @@ public class MainActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
 
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Default floating action button
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -58,6 +111,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Default coordinates when opening app...should be facebook or location data by default
+        locationData.put("Latitude",40);
+        locationData.put("Longitude",-105);
+        //TODO: display Snackbar showing current radius
+
     }
 
     @Override
@@ -119,6 +179,125 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(new LatLng(locationData.get("Latitude"),
+                locationData.get("Longitude"))).title("Marker"));
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+        mMap.setOnCameraIdleListener(this);
+
+        //Location location = mMap.getMyLocation();
+        //zoomToLocation(location);
+        mMap.setOnMyLocationChangeListener(myLocationChangeListener);
+
     }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onCameraIdle() {
+        //Snackbar mySnackbar = Snackbar.make(findViewById(R.layout.drawer_layout), mMap.getCameraPosition().toString(), Snackbar.LENGTH_LONG).show();
+        //Toast.makeText(this, mMap.getCameraPosition().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Before the map is ready many calls will fail.
+     * This should be called on all entry points that call methods on the Google Maps API.
+     */
+    private boolean checkReady() {
+        if (mMap == null) {
+            //Toast.makeText(this, "Map ain't ready yet", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Click handler for clamping to Pacific button.
+     */
+    public void zoomToLocation(Location location) {
+        if (!checkReady()) {
+            return;
+        }
+        mMap.setLatLngBoundsForCameraTarget(PACIFIC);
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(PACIFIC_CAMERA));
+    }
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            //mMarker = mMap.addMarker(new MarkerOptions().position(loc));
+            if(mMap != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 14.0f));
+            }
+        }
+    };
+
+
+
 }
